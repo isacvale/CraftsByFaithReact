bd0c57b1f921a89147d7e441bd9b118d76c89819
isacvale <isac.dvo@ocastudios.com>
2021-01-22 23:51:31 -0800

    Begins work on add-to-car options on product screen.

--- src/Components/AddToCart.js
+++ src/Components/AddToCart.js
@@ -1,9 +1,61 @@
 import React from 'react'
 import './AddToCart.css'
 
+const AddToCardOption = props => {
+    const { label, stock = 0, custom } = props
+    return (
+        <div className='AddToCard_Option'>
+            <label>
+                <input
+                    type='radio'
+                    name='variant'
+                    value={label}
+                />
+                <span>{label}</span>
+            </label>
+            { custom
+                ? null
+                : <div className='AddToCard_Option_Stock'>{stock} in stock</div>
+            }
+            
+        </div>
+    )
+}
+
 const AddToCart = () => {
     return (
-        <div className='AddToCard'>AddToCart</div>
+        <form className='AddToCard'>
+            <section className='AddToCard_PickOption'>
+                <h1 className='AddToCard_SectionTitle'>Choose option</h1>
+                <div className='AddToCard_OptionList'>
+
+                    <AddToCardOption label='Red' stock='1' />
+                    <AddToCardOption label='Blue' stock='2' />
+                    <AddToCardOption label='Yellow' stock='3' />
+                    <AddToCardOption label='Custom request' custom/>
+                    {
+                        <label>
+                            <span className='AddToCard_SectionTitle'>Please detail your request</span>
+                            <textarea className='AddToCard_CustomText interactive textInput' />
+                        </label>
+                    }
+
+                </div>
+            </section>
+
+            <section className='AddToCard_PickInitials'>
+                <label>
+                    <span className='AddToCard_SectionTitle'>Choose embroidered initials</span>
+                    <input type='text' className='interactive textInput' />
+                </label>
+            </section>
+
+            <section className='AddToCard_Buttons'>
+                <button>Add to cart</button>
+                <button>Add another to cart</button>
+                <button>Remove from cart</button>                
+            </section>
+        </form>
     )
 }
 
